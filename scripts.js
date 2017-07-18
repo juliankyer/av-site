8cfa0b865097788f209f44467360602380cb3274
Julian Kyer <julian.kyer@gmail.com>
2017-07-17 22:19:58 -0600

    Add scrolling animation on links, positioner div for for down arrow breaks links on narrow screens

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 console.log('This website was built by Julian Kyer. Inquiries at julian.kyer@gmail.com.');
+
+$(document).ready(function() {
+  $('.nav-item').on('click', function(e) {
+    e.preventDefault();
+    
+    var target = this. hash;
+    var $target = $(target);
+    
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function() {
+      window.location.hash = target;
+    });
+  });
+});
